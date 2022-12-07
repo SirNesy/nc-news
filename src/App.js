2ecef144c827b9dbdda4759cf19194f3372eaefd
nesy <umoizeonesi@gmail.com>
2022-12-07 09:38:27 +0000

    this commit renders article on another route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Articles from "./components/Articles";
 import Header from "./components/Header";
+import Article from "./components/Article";
 
 function App() {
   return (
     <div className="App">
-      <Header> News</Header>
-      <Articles />
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Articles />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:article_id" element={<Article />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getArticles } from "../utills/api";
+import { Link } from "react-router-dom";
+// import Article from "./Article";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
@@ -15,18 +17,22 @@ const Articles = () => {
   return loading ? (
     <h2>Loading...</h2>
   ) : (
-    <ul className="articles">
-      {articles.map((article) => {
-        return (
-          <li key={article.article_id} className="article">
-            <h2 className="article-title">{article.title}</h2>
-            <h2 className="article-topic">{article.topic}</h2>
-            <h4 className="article-author"> Author: {article.author}</h4>
-            <p className="article.body">{article.body}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <ul className="articles">
+        {articles.map(({ article_id, title, topic, author, body }) => {
+          return (
+            <li key={article_id} className="article">
+              <Link to={`/articles/${article_id}`}>
+                <h2 className="article-title">{title}</h2>
+              </Link>
+              <h2 className="article-topic">{topic}</h2>
+              <h4 className="article-author"> Author: {author}</h4>
+              <p className="article.body">{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
